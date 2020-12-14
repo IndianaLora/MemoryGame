b8f90c92e3fd52755ee0fc179b5cedf9c3aef2e7
Indiana Lora <indiana@ensaludybelleza.com>
2020-12-14 16:59:39 -0400

    changes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ var cards = [
   { front: "img/front.jpg" },
   { front: "img/front.jpg" },
   { front: "img/front.jpg" },
+  { front: "img/front.jpg" },
+  { front: "img/front.jpg" },
+ 
 ];
 
 var board = document.getElementById("board");
@@ -13,7 +16,7 @@ var board = document.getElementById("board");
 function boardx() {
   for (let i = 0; i < cards.length; i++) {
     var img = document.createElement("img");
-    img.src = cards.front;
+    img.src = "img/front.jpg";
     img.id = "imgStyle";
     board.appendChild(img);
   }
